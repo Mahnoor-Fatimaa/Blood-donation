3d64f46d6c77c8993149cd63ea674ddad1d01e9b
Mahnoor Fatima <mahnoor2970@gmail.com>
2025-12-13 20:55:30 +0500

    API

--- Frontend/blood-frontend/src/services/api.jsx
+++ Frontend/blood-frontend/src/services/api.jsx
@@ -1,4 +1,5 @@
-const API_URL = "http://127.0.0.1:8000";
+// const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://bloodcare.onrender.com";
 
 async function handleResponse(res) {
   if (!res.ok) {
